e8e4dceaa811bd0b635051ed7fb5127e84f78671
Tamara Temple <tamouse@gmail.com>
2014-12-27 16:40:39 -0600

    Ignore TAGS, some quick mods to lib/scrapers.rb

--- lib/scrapers.rb
+++ lib/scrapers.rb
@@ -1,16 +1,15 @@
 require 'mechanize'
 require 'uri'
-Dir[File.join(File.expand_path('../', __FILE__),'**','*.rb')].each {|file| require file}
 
 module Scrapers
   def self.agent()
     @agent ||= Mechanize.new
   end
 
-  def self.base(url)
+  def self.base_url(url)
     u = URI.parse(url)
     u.path=''
     u.to_s
   end
-
+  alias_method :base, :base_url
 end
