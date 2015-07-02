c3702ce3b5c7e09289cd646ada07e7aad5f2c137
Tamara Temple <tamouse@gmail.com>
2015-07-01 20:54:25 -0500

    Bump to version 3.1.0

--- lib/scrapers/version.rb
+++ lib/scrapers/version.rb
@@ -2,11 +2,11 @@ module Scrapers
   module Version
 
     MAJOR = 3
-    MINOR = 0
+    MINOR = 1
     BUILD = 0
 
   end
-  
+
   VERSION = [Version::MAJOR,Version::MINOR,Version::BUILD].map(&:to_s).join(".")
 
   DESCRIPTION = "A library of web site scrapers utilizing mechanize and other goodies. Helpful in gathering images, moving things, saving things, etc."
